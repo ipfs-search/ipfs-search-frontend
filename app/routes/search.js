9b57516381376b54f6be45abb0abd3886d79bc54
Aad Versteden <madnificent@gmail.com>
2018-02-02 11:40:14 +0100

    Fetch the search string from the URL
    The URL is the basic location to store all state.  The search string
    is now stored in a query parameter, as it used to be.

--- app/routes/search.js
+++ app/routes/search.js
@@ -2,11 +2,15 @@ import { inject } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  queryParams: {
+    q: { refreshModel: true }
+  },
   ajax: inject(),
-  model( { } ) {
+  model( { q: searchString } ) {
+    console.log(`Searching for ${searchString}`);
     return this.get('ajax').request('/v1/search', {
       method: 'GET',
-      data: { q: "hello" }
+      data: { q: searchString }
     });
   }
 });
