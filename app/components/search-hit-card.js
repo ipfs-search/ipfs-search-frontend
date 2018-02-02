3945755409678eb21430c55e9265da1934adf390
Aad Versteden <madnificent@gmail.com>
2018-02-02 23:45:52 +0100

    Allow all hits to be expanded with one option
    It may be preferred to open the hit cards from the get-go.  Check the
    new box at the top to enable this use-case.

--- app/components/search-hit-card.js
+++ app/components/search-hit-card.js
@@ -1,6 +1,10 @@
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
+  cardOpen: computed( 'more', 'forceExpand', function() {
+    return this.get('more') || this.get('forceExpand');
+  }),
   actions: {
     showMore(){
       this.toggleProperty('more');
