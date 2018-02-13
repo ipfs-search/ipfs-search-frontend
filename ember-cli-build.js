a20ade6e8c8621d990de1084734d3e94e048dbe2
Aad Versteden <madnificent@gmail.com>
2018-02-13 10:21:38 +0100

    Install ember-bootstrap v4
    This includes ember-bootstrap version 4.  We chose to place the
    ember-wormhole statement into the index ourselves so we can move it
    around as necessary.  We're using the sass version, which should make
    it easier for use to style various components.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
+      'importBootstrapCSS': false,
+      'insertEmberWormholeElementToDom': false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
