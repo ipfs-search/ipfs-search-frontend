c7dae2c3e9be31b5f218fcaec8cf24cc2efe6be9
Florian Kaiser <florian.kaiser@fnkr.net>
2018-02-16 20:03:43 +0100

    Use local IPFS gateway if available

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,5 +1,6 @@
 import { inject } from '@ember/service';
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   queryParams: {
@@ -15,6 +16,7 @@ export default Route.extend({
   },
   search: null,
   ajax: inject(),
+  defaultGateway: 'https://gateway.ipfs.io',
   model( { search, page, kind } ) {
     if( search || kind ) {
       if( ! search ) { search = ""; }
@@ -37,13 +39,32 @@ export default Route.extend({
         }
       }
 
-      return this.get('ajax').request('http://api.ipfs-search.com/v1/search', {
+      let response = this.get('ajax').request('http://api.ipfs-search.com/v1/search', {
         method: 'GET',
         data: { q: search, page: page, _type: fileOrDirectory }
       }).catch( (err) => {
         window.lasterr = err;
         throw err;
       } );
+
+      let gateway = this.get('ajax').raw('http://localhost:8080/ipfs/QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv/ping', {
+        method: 'GET',
+        cache: false,
+        timeout: 50,
+        dataType: 'text',
+      }).catch( () => {
+        return this.defaultGateway;
+      }).then( (response) => {
+        if (response.payload !== 'ipfs') {
+          return this.defaultGateway;
+        }
+        return 'http://localhost:8080';
+      });
+
+      return RSVP.hash({
+        response: response,
+        gateway: gateway
+      });
     } else {
       return null;
     }
