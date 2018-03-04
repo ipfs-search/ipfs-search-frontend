3b563df5211ad1d04dda1a6215497d69435aecfb
Aad Versteden <madnificent@gmail.com>
2018-03-04 18:33:33 +0100

    Create search-identity component
    There was a template file laying around which wasn't used.  By moving
    this into a component, we can use it as desired.
    
    Ember generators are an easy way to create the right components.  More
    information on
    https://guides.emberjs.com/v3.0.0/components/defining-a-component/

---
+++ tests/integration/components/search-identity-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | search-identity', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{search-identity}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      {{#search-identity}}
+        template block text
+      {{/search-identity}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
