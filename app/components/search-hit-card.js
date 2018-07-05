85f05668f0b5b9e1ce84355f8c6323fe587ac7e7
Aad Versteden <madnificent@gmail.com>
2018-07-05 09:17:34 +0200

    Render searches as per design
    The design gives some indication as to how results should be
    rendered.  Although we don't always have the necessary information at
    hand, we're now rendering the results themselves based on the design.

--- app/components/search-hit-card.js
+++ app/components/search-hit-card.js
@@ -2,12 +2,17 @@ import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
+  tagName: '',
+
   cardOpen: computed( 'more', 'forceExpand', function() {
     return this.get('more') || this.get('forceExpand');
   }),
   actions: {
     showMore(){
       this.toggleProperty('more');
+    },
+    openModal(){
+      this.toggleProperty('openModal');
     }
   }
 });
