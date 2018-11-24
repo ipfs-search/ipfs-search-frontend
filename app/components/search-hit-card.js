89c04947b7a4fdd9f384180c0b1d6a9d951d452f
Aad Versteden <madnificent@gmail.com>
2018-11-24 22:01:53 +0100

    Use color coding to indicate freshness of results
    Kees has designed a nice system for indicating the availability.  This
    is now embedded in the generated results.

--- app/components/search-hit-card.js
+++ app/components/search-hit-card.js
@@ -1,5 +1,6 @@
 import { computed } from '@ember/object';
 import Component from '@ember/component';
+import moment from 'moment';
 
 export default Component.extend({
   tagName: '',
@@ -7,6 +8,40 @@ export default Component.extend({
   cardOpen: computed( 'more', 'forceExpand', function() {
     return this.more || this.forceExpand;
   }),
+  availabilityClass: computed( 'hit.last-seen', function() {
+    if( this.get('hit.last-seen') ) {
+      const then = moment( this.get('hit.last-seen') );
+      const timeDiff = moment().diff( then );
+
+      const duration = moment.duration( timeDiff );
+      if( duration < moment.duration( { months: 1 } ) ) {
+        return "available";
+      } else if( duration < moment.duration( { months: 6 } ) ) {
+        return "might-be-available";
+      } else {
+        return "unavailable";
+      }
+    } else {
+      // return "availability-unknown";
+      return "unavailable";
+    }
+  }),
+  faIcon: computed( 'kind', function() {
+    if( this.kind == "image" )
+      return "fa-image";
+    else if( this.kind == "video" )
+      return "fa-video";
+    else if( this.kind == "audio" )
+      return "fa-volume-up";
+    else if( this.kind == "directory" )
+      return "fa-folder-open";
+    else if( this.kind == "any" )
+      return "fa-file-alt";
+    else if( this.kind == "file" )
+      return "fa-file";
+    else
+      return "fa-file-alt"; // fallback is same as any
+  } ),
   actions: {
     showMore(){
       this.toggleProperty('more');
