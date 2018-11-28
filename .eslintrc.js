c850e11cb3722e6bff3964f9543c1a45d7d94774
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-11-28 21:29:34 +0000

    Ember dist from IPFS.

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
+  env: {
+    browser: true
+  },
+  rules: {
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    },
+
+    // test files
+    {
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      }
+    }
+  ]
+};
---
+++ .template-lintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'attribute-indentation': false,
+    'no-invalid-interactive': false,
+    'self-closing-void-elements': false
+  }
+};
