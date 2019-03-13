a30c18570ed75d138b30cc0e1c93e44141a2e145
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-03-13 09:53:34 +0000

    Automated rendering.

---
+++ render.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+    # Working directory clean
+    npm install
+    ember build --environment production
+    git checkout v2_rendered
+    cp -r dist/* .
+    git add assets
+    git commit -a -m 'Updated render'
+    git push
+    git reset --hard
+    git checkout v2
+else
+    # Uncommitted changes
+    echo 'Uncommitted changes. Cowardly refusing to operate'
+    exit -1
+fi
+
