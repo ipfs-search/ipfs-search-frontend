756796b6bc1be83306cb1253d8707dbf71769050
Mehran Naghizadeh <tarhineh@gmail.com>
2019-03-16 23:58:39 +0100

    Expose the function to other parts of the project

---
+++ app/utils/humanize-bytes.js
@@ -0,0 +1,9 @@
+// Taken from https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+export default function humanizeBytes(bytes,decimals) {
+  if(bytes == 0) return '0 Bytes';
+  var k = 1024,
+      dm = decimals <= 0 ? 0 : decimals || 2,
+      sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+      i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
