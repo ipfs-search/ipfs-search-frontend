32065abda9dc1227b011ff4a774913e3908076b1
Mehran Naghizadeh <tarhineh@gmail.com>
2019-03-16 23:04:59 +0100

    Human readable file sizes

--- app/components/search-hit-card.js
+++ app/components/search-hit-card.js
@@ -46,6 +46,10 @@ export default Component.extend({
       return "any";
     }
   }),
+  humanizedSize: computed ( 'hit.size', function(){
+    function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
+    return formatBytes(parseInt(this.get('hit.size')));
+  }),
   faIcon: computed( 'kind', function() {
     if( this.kind == "image" )
       return "fa-image";
