643a0201da3ecd5dafa87b227b22d5c728e730c3
Aad Versteden <madnificent@gmail.com>
2020-05-12 15:27:54 +0200

    Update name to cloudflaire-ipfs.com
    Copypasting from the issue without doublechecking is a bad idea.

--- app/helpers/hit-urls.js
+++ app/helpers/hit-urls.js
@@ -3,6 +3,6 @@ import { helper } from '@ember/component/helper';
 export default helper(function hitUrls([hit]/*, hash*/) {
   return [
     `https://gateway.ipfs.io/ipfs/${hit.hash}`,
-    `https://clowdflare-ipfs.com/ipfs/${hit.hash}`
+    `https://cloudflare-ipfs.com/ipfs/${hit.hash}`
   ];
 });
