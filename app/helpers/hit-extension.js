9f4bdaedc6d38c48288c50795c3747bae6c5aaa7
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-10-28 11:47:42 +0000

    Fix silly bug in ext.

--- app/helpers/hit-extension.js
+++ app/helpers/hit-extension.js
@@ -3,5 +3,10 @@ import { helper } from '@ember/component/helper';
 import mime from 'mime';
 
 export default helper(function hitExtension([hit]) {
-  return mime.getExtension(hit.mimetype).toUpperCase();
+  let ext = mime.getExtension(hit.mimetype)
+
+  // Override mpga extension with mp3.
+  if (ext == 'mpga') ext = 'mp3';
+
+  return ext && ext.toUpperCase();
 });
