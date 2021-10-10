a6c203a1a1bd0b26d7728810789e72b7067b76cc
Bram Neijt <bram@neijt.nl>
2021-10-10 15:56:47 +0200

    Add copy has button

--- app/components/search-hit/details.js
+++ app/components/search-hit/details.js
@@ -1,5 +1,10 @@
 import Component from '@glimmer/component';
 
 export default class SearchHitDetailsComponent extends Component {
-
+    actions = {
+        async copyHitHash() {
+            await navigator.clipboard.writeText(this.args.hit.hash);
+            return false;
+        }
+    }
 }
